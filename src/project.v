961290d9757b670c549ebce393c936e8ffa2858e
AbdelmoumenBacetti <46109536+AbdelmoumenBacetti@users.noreply.github.com>
2024-04-18 17:28:01 +0100

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,20 +5,30 @@
 
 `default_nettype none
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
-);
-
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-endmodule
+module tt_um_AbdelmoumenBacetti_aes(
+           input  wire [7:0] ui_in,    // Dedicated inputs
+           output wire [7:0] uo_out,   // Dedicated outputs
+           input  wire [7:0] uio_in,   // IOs: Input path
+           output wire [7:0] uio_out,  // IOs: Output path
+           output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+           input  wire       ena,      // will go high when the design is enabled
+           input  wire       clk,      // clock
+           input  wire       rst_n     // reset_n - low to reset
+           );
+           
+wire aes_we = uio_oe[0];
+//wire [7 : 0] aes_address = ;
+//wire  [31 : 0] aes_write_data = ;
+//wire [31 : 0] aes_read_data = ;
+           
+aes aes_dut(
+           .clk(clk),
+           .reset_n(reset_n),
+           .cs(ena),
+           .we(aes_we),
+           .address(aes_address),
+           .write_data(aes_write_data),
+           .read_data(aes_read_data)
+           );
+ 
+endmodule // tt_um_AbdelmoumenBacetti_aes
